318c1a30b7b3c0717441b148f09e94742ab18bdd
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2025-07-16 16:24:06 +0200

    fix: exchange children  in  blog section

--- src/components/blog-section/BlogSection.tsx
+++ src/components/blog-section/BlogSection.tsx
@@ -18,6 +18,7 @@ export const BlogSection = () => (
             key={i}
             to={`${i}`}
             iconClassName='hidden'>
+            <div />
             {/* <NewCard /> */}
           </Link>
         ))}
