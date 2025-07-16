500195c019658a6b0d3975acab9d7d4e7bc97a8f
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2025-07-16 16:33:54 +0200

    chore: add  eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,13 @@
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tailwindcss from 'eslint-plugin-tailwindcss'
-import tsparser from '@typescript-eslint/parser'
-import tseslint from '@typescript-eslint/eslint-plugin'
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
-export default {
-  languageOptions: {
-    parser: tsparser,
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    globals: globals.browser
-  },
-  plugins: {
-    '@typescript-eslint': tseslint,
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-    tailwindcss
-  },
-  rules: {
-    'no-unused-vars': 'warn',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off'
-  }
-}
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,32 +1,32 @@
-import { useTranslation } from "react-i18next";
-import { FaqSection, ProductsSection, SelectedProducts } from "../components";
-import { OurAdvantagesSection, ReviewsSection } from "../block-pages/home";
+import { useTranslation } from 'react-i18next'
+import { FaqSection, ProductsSection, SelectedProducts } from '../components'
+// import { OurAdvantagesSection, ReviewsSection } from "../block-pages/home";
 
 type ListQuestion = {
-  question: string;
-  answer: string;
-};
+  question: string
+  answer: string
+}
 
 export const ProductPage = () => {
-  const { t } = useTranslation("translation", {
-    keyPrefix: "DeliveryPage",
-  });
+  const { t } = useTranslation('translation', {
+    keyPrefix: 'DeliveryPage'
+  })
 
-  const listQuestion = t("deliver-faq-section.list-question", {
-    returnObjects: true,
-  }) as ListQuestion[];
+  const listQuestion = t('deliver-faq-section.list-question', {
+    returnObjects: true
+  }) as ListQuestion[]
 
   return (
     <>
       <ProductsSection />
       <SelectedProducts />
-      <OurAdvantagesSection className="hidden" />
-      <ReviewsSection />
+      {/* <OurAdvantagesSection className="hidden" />
+      <ReviewsSection /> */}
       <FaqSection
-        title={t("deliver-faq-section.title")}
+        title={t('deliver-faq-section.title')}
         listQuestion={listQuestion}
         isProductPage={true}
       />
     </>
-  );
-};
+  )
+}
